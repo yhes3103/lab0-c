e376000e06f3cdbc99226d9cd0c02628e03c7859
Michelle Wang <yhes3103@gmail.com>
2025-09-16 01:58:50 +0800

    Implement basic queue functions
    Add q_new, q_insert_head, q_insert_tail, q_remove_head, q_remove_tail, and q_size with initial tests passing.
    
    Change-Id: I8b73dfa5d65a055fff6c6aeb54b20d0a42715b5a

--- queue.c
+++ queue.c
@@ -4,43 +4,130 @@
 
 #include "queue.h"
 
+/* Check if empty or not */
+static inline bool q_empty(struct list_head *head)
+{
+    return head->next == head;
+}
+
+/* copy the string to the caller's buffer*/
+static inline void copy_out(char *dst, size_t n, const char *src)
+{
+    if (!dst || n == 0)
+        return;
+    if (!src) {
+        dst[0] = '\0';
+        return;
+    }
+    strncpy(dst, src, n - 1);
+    dst[n - 1] = '\0';
+}
+
+
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *pos, *n;
+    list_for_each_safe(pos, n, head) {
+        element_t *e = list_entry(pos, element_t, list);
+        list_del(pos);         // remove first
+        q_release_element(e);  // then free e-> value and e
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *e = malloc(sizeof(*e));
+    if (!e)
+        return false;
+
+    char *dup = s ? strdup(s) : strdup("");
+    if (!dup) {
+        free(e);
+        return false;
+    }
+    e->value = dup;
+    INIT_LIST_HEAD(&e->list);
+    list_add(&e->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *e = malloc(sizeof(*e));
+    if (!e)
+        return false;
+
+    char *dup = s ? strdup(s) : strdup("");
+    if (!dup) {
+        free(e);
+        return false;
+    }
+    e->value = dup;
+    INIT_LIST_HEAD(&e->list);
+    list_add_tail(&e->list, head);
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || q_empty(head))
+        return NULL;
+
+    struct list_head *node = head->next;
+    element_t *e = list_entry(node, element_t, list);
+    list_del(node);
+    copy_out(sp, bufsize, e->value);
+    return e;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || q_empty(head))
+        return NULL;
+
+    struct list_head *node = head->prev;
+    element_t *e = list_entry(node, element_t, list);
+    list_del(node);
+    copy_out(sp, bufsize, e->value);
+    return e;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || q_empty(head))
+        return 0;
+
+    int cnt = 0;
+    struct list_head *pos;
+    list_for_each(pos, head)
+        cnt++;
+    return cnt;
 }
 
 /* Delete the middle node in queue */
